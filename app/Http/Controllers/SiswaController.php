10447e82e4890666586e62308df9f7a4c9380a44
Julianhendrawan <julianhendrawan1@gmail.com>
2021-08-30 22:07:43 +0700

    laravel-crud

---
+++ app/Http/Controllers/SiswaController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SiswaController extends Controller
+{
+    public function index() 
+    {
+        $data_siswa = \App\Models\Siswa::all();
+        return view('siswa.index',['data_siswa' => $data_siswa]);
+    }
+
+    public function create(Request $request)
+    {
+        \App\Models\Siswa::create($request->all());
+        return redirect('/siswa')->with('sukses','Data berhasil dimasukan!');
+    }
+
+    public function edit($id)
+    {
+        $siswa = \App\Models\Siswa::find($id);
+        return view('siswa/edit',['siswa' => $siswa]);
+        // return redirect('/siswa')->with('sukses','Data berhasil dimasukan!');
+    }
+
+    public function update(Request $request,$id)
+    {
+        $siswa = \App\Models\Siswa::find($id);
+        $siswa->update($request->all());
+        return redirect('/siswa')->with('sukses','Data berhasil diupdate!');
+    }
+
+    public function delete($id)
+    {
+        $siswa = \App\Models\Siswa::find($id);
+        $siswa->delete($siswa);
+        return redirect('/siswa')->with('sukses','Data berhasil dihapus!');
+    }
+}
